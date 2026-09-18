43d0f003b54e3df4ec9f8031d6236f4ed48af01e
cordeiroalfa0-dev <2.31991193e+08+cordeiroalfa0-dev@users.noreply.github.com>
2026-09-18 01:14:43 +0000

    fix: prevent overlapping player controls on web and mobile

--- mame-web.js
+++ mame-web.js
@@ -233,7 +233,7 @@
     playerUrl.searchParams.set("name", cleanRomName(romName));
     // O player é alterado junto com o bridge; versionar a URL evita que o
     // navegador reutilize uma versão antiga que ainda exibia o menu RetroArch.
-    playerUrl.searchParams.set("v", "20260915-fullscreen-fill-fix");
+    playerUrl.searchParams.set("v", "20260918-controls-no-overlap-v1");
     if (biosUrl) playerUrl.searchParams.set("bios", biosUrl);
     if (biosName) playerUrl.searchParams.set("biosName", biosName);
 
